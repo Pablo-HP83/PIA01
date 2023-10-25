5def6652155c715d35a9d690497e8a48ddfe12b6
Pablo HP - BDIA <149018975+Pablo-HP83@users.noreply.github.com>
2023-10-26 00:32:12 +0200

    Create hola_mundo.py

---
+++ hola_mundo.py
@@ -0,0 +1,8 @@
+# importo init y Fore del módulo colorama para dar color a la salida del print
+from colorama import init, Fore
+
+# inicializo colorama
+init()
+
+# imprimo "Hola Mundo" en rojo
+print(Fore.RED + "Hola Mundo")
